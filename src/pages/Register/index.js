95a7701471b262f69b7fc0580349835c747952c0
Juliano <julianodesenv@gmail.com>
2021-08-10 15:30:03 -0300

    create pages

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,9 +1,9 @@
 import { Container } from '../../styles/GlobalStyles';
 
-export default function Login() {
+export default function Register() {
   return (
     <Container>
-      <h1>Login</h1>
+      <h1>Cadastro</h1>
     </Container>
   );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,21 @@ import { Switch } from 'react-router-dom';
 
 import MyRoute from './MyRoute';
 import Login from '../pages/Login';
+import Student from '../pages/Student';
+import Students from '../pages/Students';
+import Photos from '../pages/Photos';
+import Register from '../pages/Register';
 import Page404 from '../pages/Page404';
 
 export default function Routes() {
   return (
     <Switch>
-      <MyRoute exact path="/" component={Login} />
+      <MyRoute exact path="/" component={Students} isClosed={false} />
+      <MyRoute exact path="/students/:id/edit" component={Student} isClosed />
+      <MyRoute exact path="/students" component={Student} isClosed />
+      <MyRoute exact path="/photos/:id" component={Photos} isClosed />
+      <MyRoute exact path="/register" component={Register} isClosed={false} />
+      <MyRoute exact path="/login" component={Login} isClosed={false} />
       <MyRoute path="*" component={Page404} />
     </Switch>
   );
